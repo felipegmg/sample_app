bee73d4eeeb024e4f6818c492fceb5736e5d2c19
Felipe Garcia <felipegmg@id.uff.br>
2011-06-27 17:21:12 -0300

    Static pages finalizada

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,15 @@
 class PagesController < ApplicationController
+
   def home
+    @title = "Principal"
   end
 
   def contact
+    @title = "Fale conosco"
   end
 
+  def about
+    @title = "Sobre"
+  end
 end
+
